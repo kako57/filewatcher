facee93cf9e5a2c085093d9b10cc82c28cf0dbd2
Dre Cura <ajlcura@hotmail.com>
2021-04-18 20:04:25 -0400

    using tabs now; added support for clang++

--- src/main.rs
+++ src/main.rs
@@ -7,97 +7,128 @@ use std::process::{Command, Stdio};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
-fn try_compile(file: PathBuf) {
-    let cxxflags = ["-fdiagnostics-color=always", "-O2", "-Wall", "-std=c++17"];
-    if file.is_file() {
-        if let Some(ext) = file.extension() {
-            if ext == "cpp" || ext == "cc" || ext == "c" {
-                let output = Command::new("g++")
-                    .args(&cxxflags)
-                    .arg(&file)
-                    .arg("-o")
-                    .arg(file.file_stem().unwrap())
-                    .stderr(Stdio::piped())
-                    .output();
-                let output = match output {
-                    Ok(o) => o,
-                    Err(_) => {
-                        println!("failed to run g++\ndo you even have g++?");
-                        return (); // exits function immediately
-                    }
-                };
-                if output.stderr.len() > 0 {
-                    println!("{}", String::from_utf8(output.stderr).expect("not UTF-8"));
-                } else {
-                    println!("{:?} was compiled successfully", file);
-                }
-            }
-        }
-    }
+fn try_compile(file: PathBuf, compiler: &str) {
+	let gppflags =
+		vec!["-fdiagnostics-color=always", "-O2", "-Wall", "-std=c++17"];
+	let clangflags = vec!["-fcolor-diagnostics", "-O2", "-Wall", "-std=c++17"];
+
+	let flags = match compiler {
+		"g++" => gppflags,
+		"clang++" => clangflags,
+		_ => Vec::new(),
+	};
+
+	if file.is_file() {
+		if let Some(ext) = file.extension() {
+			if ext == "cpp" || ext == "cc" || ext == "c" {
+				let output = Command::new(&compiler)
+					.args(&flags)
+					.arg(&file)
+					.arg("-o")
+					.arg(file.file_stem().unwrap())
+					.stderr(Stdio::piped())
+					.output();
+
+				let output = match output {
+					Ok(o) => o,
+					Err(_) => {
+						println!("failed to run the compiler");
+						return; // exit function immediately
+					}
+				};
+
+				if output.stderr.is_empty() {
+					println!("{:?} was compiled successfully", file);
+				} else {
+					println!(
+						"{}",
+						String::from_utf8(output.stderr).expect("not UTF-8")
+					);
+				}
+			}
+		}
+	}
 }
 
-fn handle(event: DebouncedEvent) {
-    match event {
-        DebouncedEvent::Write(file) => {
-            println!("WRITE: {:?} is written", &file);
-            try_compile(file);
-        }
-        DebouncedEvent::Create(file) => {
-            println!("CREATE: {:?} is created", &file);
-            try_compile(file);
-        }
-        DebouncedEvent::Remove(file) => {
-            println!("REMOVE: {:?} was removed", file);
-        }
-        DebouncedEvent::Rename(oldname, newname) => {
-            println!("RENAME {:?} was renamed to {:?}", oldname, newname);
-        }
-        DebouncedEvent::Chmod(file) => {
-            println!("CHMOD: file permissions of {:?} has been changed", file);
-        }
-        DebouncedEvent::Rescan => {
-            println!("RESCAN: problem detected\nrescanned file/directory.");
-        }
-        DebouncedEvent::Error(e, path) => {
-            println!("ERROR: error {:?} at {:?}", e, path);
-        }
-        _ => {}
-    }
+fn handle(event: DebouncedEvent, compiler: &str) {
+	match event {
+		DebouncedEvent::Write(file) => {
+			println!("WRITE: {:?} is written", &file);
+			try_compile(file, compiler);
+		}
+		DebouncedEvent::Create(file) => {
+			println!("CREATE: {:?} is created", &file);
+			try_compile(file, compiler);
+		}
+		DebouncedEvent::Remove(file) => {
+			println!("REMOVE: {:?} was removed", file);
+		}
+		DebouncedEvent::Rename(oldname, newname) => {
+			println!("RENAME {:?} was renamed to {:?}", oldname, newname);
+		}
+		DebouncedEvent::Chmod(file) => {
+			println!("CHMOD: file permissions of {:?} has been changed", file);
+		}
+		DebouncedEvent::Rescan => {
+			println!("RESCAN: problem detected\nrescanned file/directory.");
+		}
+		DebouncedEvent::Error(e, path) => {
+			println!("ERROR: error {:?} at {:?}", e, path);
+		}
+		_ => {}
+	}
 }
 
 fn main() {
-    let mut args = env::args().skip(1);
+	let mut args = env::args().skip(1);
 
-    let path_or_file = match args.next() {
-        Some(p) => p,
-        _ => String::from("."),
-    };
+	let path_or_file = match args.next() {
+		Some(p) => p,
+		_ => String::from("."),
+	};
 
-    let root = Path::new(&path_or_file);
-    match env::set_current_dir(&root) {
-        Err(_) => {
-            println!("Invalid file or directory");
-            return ();
-        }
-        _ => {}
-    }
+	let root = Path::new(&path_or_file);
+	if env::set_current_dir(&root).is_err() {
+		println!("Invalid file or directory");
+		return;
+	}
 
-    println!("Setting up watcher.");
+	let possible_compilers = vec!["g++", "clang++"];
 
-    let (tx, rx) = channel();
+	let mut compiler = "";
 
-    let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
+	for c in possible_compilers {
+		let test = Command::new("which").arg(c).output();
+		if test.is_ok() {
+			compiler = c;
+		}
+	}
 
-    watcher
-        .watch(path_or_file, RecursiveMode::Recursive)
-        .unwrap();
+	if compiler.is_empty() {
+		println!("you don't have g++ or clang++ installed.");
+		return;
+	}
+	println!(
+		"The watcher will use {} as the compiler for C/C++",
+		&compiler
+	);
 
-    println!("Setup success!");
+	println!("Setting up watcher.");
 
-    loop {
-        match rx.recv() {
-            Ok(event) => handle(event),
-            Err(e) => println!("watch error: {:?}", e),
-        }
-    }
+	let (tx, rx) = channel();
+
+	let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
+
+	watcher
+		.watch(path_or_file, RecursiveMode::Recursive)
+		.unwrap();
+
+	println!("Watcher setup success!");
+
+	loop {
+		match rx.recv() {
+			Ok(event) => handle(event, compiler),
+			Err(e) => println!("watch error: {:?}", e),
+		}
+	}
 }
